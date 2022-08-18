94cf9ab7f32507059dbc197db5c554a917c4998d
TresYap <tresyap.work@gmail.com>
2022-08-18 19:22:16 +0800

    feat: added sakaydb module with empty functions.

---
+++ sakaydb.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+class SakayDBError(ValueError):
+    pass
+
+class SakayDB():
+
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+
+    def add_trip():
+        pass
+
+    def add_trips():
+        pass
+
+    def delete_trip():
+        pass
+
+    def search_trips():
+        pass
+
+    def export_data():
+        pass
+
+    def generate_statistics():
+        pass
+
+    def plot_statistics():
+        pass
+
+    def generate_odmatrix():
+        pass
