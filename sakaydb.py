9cc68d9da78ee1ee535342b8517f5383f24d65eb
TresYap <tresyap.work@gmail.com>
2022-09-03 20:03:14 +0000

    chore: Edited code for pep8 compliance.

--- sakaydb.py
+++ sakaydb.py
@@ -3,21 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+
 class SakayDBError(ValueError):
+
      def __init__(self, message="SakayDBError"):
         self.message = message
         super().__init__(self.message)
 
+
 class SakayDB():
 
     def __init__(self, data_dir):
-        """Initializes by taking path to the data and reading the necessary csvs for SakayDB."""
+        """Initializes by taking path to the data
+        and reading the necessary csvs for SakayDB."""
         self.data_dir = data_dir
 
-    def add_trip(self, driver, pickup_datetime, dropoff_datetime, passenger_count,
-                 pickup_loc_name, dropoff_loc_name, trip_distance, fare_amount):
-        #Step -0.5: If csv files don't exist, create empty csvs* (check if this works)
-        #Step 0: Import dfs question 1: should the file always exist regardless of size?
+    def add_trip(self, driver, pickup_datetime, dropoff_datetime,
+                 passenger_count, pickup_loc_name, dropoff_loc_name,
+                 trip_distance, fare_amount):
         try:
             trips = pd.read_csv(os.path.join(self.data_dir, 'trips.csv'))
         except FileNotFoundError:
@@ -33,16 +36,17 @@ class SakayDB():
         try:
             drivers = pd.read_csv(os.path.join(self.data_dir, 'drivers.csv'))
         except FileNotFoundError:
-            drivers = pd.DataFrame(columns=['driver_id', 'given_name', 'last_name'])
-            
-        try:
-            locations = pd.read_csv(os.path.join(self.data_dir, 'locations.csv'))
-        except FileNotFoundError:
-            locations = pd.DataFrame(columns=['location_id', 'loc_name'])
-        
-        #Step 1: Convert input to trips row (function input -> expected entry)
+            drivers = pd.DataFrame(columns=['driver_id',
+                                            'given_name',
+                                            'last_name'])
 
-        #Step 1a: Get or create driver_id
+        try:
+            locations = pd.read_csv(os.path.join(self.data_dir,
+                                                 'locations.csv'))
+        except FileNotFoundError:
+            locations = pd.DataFrame(columns=['location_id',
+                                              'loc_name'])
+
         names = driver.strip().split(', ')
         last_name = names[0]
         given_name = names[1]
@@ -57,10 +61,13 @@ class SakayDB():
                 'last_name': [last_name]
             })
             drivers = pd.concat([drivers, new_row], ignore_index=True)
-        elif drivers[(drivers.given_name.str.lower() == given_name.lower()) 
-                   & (drivers.last_name.str.lower() == last_name.lower())].shape[0] > 0:
-            driver_id = (drivers[(drivers.given_name.str.lower() == given_name.lower()) 
-                                 & (drivers.last_name.str.lower() == last_name.lower())]
+        elif drivers[(drivers.given_name.str.lower() == given_name.lower())
+                     & (drivers.last_name.str.lower() == last_name.lower())
+                     ].shape[0] > 0:
+            driver_id = (drivers[(drivers.given_name.str.lower()
+                                  == given_name.lower()) &
+                                 (drivers.last_name.str.lower()
+                                  == last_name.lower())]
                          ['driver_id'].values[0])
         else:
             driver_id = drivers['driver_id'].iloc[-1] + 1
@@ -71,39 +78,36 @@ class SakayDB():
             })
             drivers = pd.concat([drivers, new_row], ignore_index=True)
 
-        #Step 1b: Get or create pickup and dropoff id
         if locations.shape[0] == 0:
             pickup_loc_id = 1
             dropoff_loc_id = 2
         else:
             if pickup_loc_name in locations.loc_name.values.tolist():
-                pickup_loc_id = (locations[locations.loc_name == pickup_loc_name]['location_id']
+                pickup_loc_id = (locations[locations.loc_name
+                                           == pickup_loc_name]
+                                 ['location_id']
                                  .values[0])
             else:
                 pickup_loc_id = locations['location_id'].iloc[-1] + 1
                 new_row = pd.DataFrame({
-                'location_id': [pickup_loc_id],
-                'loc_name': [pickup_loc_name],
-                })
+                                       'location_id': [pickup_loc_id],
+                                       'loc_name': [pickup_loc_name],
+                                       })
                 locations = pd.concat([locations, new_row], ignore_index=True)
 
             if dropoff_loc_name in locations.loc_name.values.tolist():
-                dropoff_loc_id = (locations[locations.loc_name == dropoff_loc_name]['location_id']
+                dropoff_loc_id = (locations[locations.loc_name
+                                            == dropoff_loc_name]
+                                  ['location_id']
                                   .values[0])
             else:
                 dropoff_loc_id = locations['location_id'].iloc[-1] + 1
                 new_row = pd.DataFrame({
-                'location_id': [dropoff_loc_id],
-                'loc_name': [dropoff_loc_name],
-                })
+                                       'location_id': [dropoff_loc_id],
+                                       'loc_name': [dropoff_loc_name],
+                                       })
                 locations = pd.concat([locations, new_row], ignore_index=True)
 
-        #Step 2: Insert row
-        #Step 2a: Check if row exists -> out: SakayDBError
-        # driver (case-insensitive), pickup_datetime, dropoff_datetime, passenger_count,
-        # pickup_loc_name, dropoff_loc_name, trip_distance and fare_amount
-        #TODO: check if timestamp matching necessary or if string is enough
-        
         row = {
             'driver_id': driver_id,
             'pickup_datetime': pickup_datetime,
@@ -117,21 +121,22 @@ class SakayDB():
         try:
             if trips.shape[0] == 0:
                 row['trip_id'] = 1
-            elif row in trips.loc[:, trips.columns != 'trip_id'].to_dict(orient='records'):
+            elif row in (trips.loc[:, trips.columns != 'trip_id']
+                              .to_dict(orient='records')):
                 raise SakayDBError
             else:
                 row['trip_id'] = trips['trip_id'].iloc[-1] + 1
-            trips = pd.concat([trips, pd.DataFrame.from_dict([row])], ignore_index=True)
+            trips = pd.concat([trips, pd.DataFrame.from_dict([row])],
+                              ignore_index=True)
         except SakayDBError:
             raise SakayDBError
-        #Step 2b: If row not exist, append at end of file 
-#         display(drivers) #remove this section
-#         display(locations.tail())
-#         display(trips)
-        #to_csv section
-        trips.to_csv(os.path.join(self.data_dir, 'trips.csv'), index=False)
-        drivers.to_csv(os.path.join(self.data_dir, 'drivers.csv'), index=False)
-        locations.to_csv(os.path.join(self.data_dir, 'locations.csv'), index=False)
+
+        trips.to_csv(os.path.join(self.data_dir, 'trips.csv'),
+                     index=False)
+        drivers.to_csv(os.path.join(self.data_dir, 'drivers.csv'),
+                       index=False)
+        locations.to_csv(os.path.join(self.data_dir, 'locations.csv'),
+                         index=False)
 
         return trips['trip_id'].iloc[-1]
 
