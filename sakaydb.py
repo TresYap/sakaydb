e16e060c3a3ad6658d7e4c813f67485974e88f56
TresYap <tresyap.work@gmail.com>
2022-09-10 12:22:30 +0000

    fix: fixed pep8 comments.

--- sakaydb.py
+++ sakaydb.py
@@ -3,13 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+
 class SakayDBError(ValueError):
     pass
 
 class SakayDB():
 
     def __init__(self, data_dir):
-        """Initializes by taking path to the data and reading the necessary csvs for SakayDB."""
+        """Initializes by taking path
+        to the data and reading the necessary csvs for SakayDB."""
         self.data_dir = data_dir
 
     def add_trip(self):
@@ -26,30 +28,43 @@ class SakayDB():
 
     def export_data(self):
         """
-        Merges trips.csv, drivers.csv, locations.csv 
+        Merges trips.csv, drivers.csv, locations.csv
         and returns a table containing all the columns from each.
 
         Returns
         -------
         """
-        if (not os.path.isfile(os.path.join(self.data_dir, 'trips.csv'))
-            or not os.path.isfile(os.path.join(self.data_dir, 'drivers.csv'))
-            or not os.path.isfile(os.path.join(self.data_dir, 'locations.csv'))):
-            df = pd.DataFrame(columns=['dropoff_loc_name', 'passenger_count', 'trip_distance',
-                                       'dropoff_datetime', 'fare_amount', 'driver_lastname',
-                                       'pickup_loc_name', 'driver_givenname', 'pickup_datetime'])
+        if (not os.path.isfile(os.path.join(self.data_dir,
+                                            'trips.csv'))
+                or not os.path.isfile(os.path.join(self.data_dir,
+                                                   'drivers.csv'))
+                or not os.path.isfile(os.path.join(self.data_dir,
+                                                   'locations.csv'))):
+            df = pd.DataFrame(columns=['dropoff_loc_name', 'passenger_count',
+                                       'trip_distance', 'dropoff_datetime',
+                                       'fare_amount', 'driver_lastname',
+                                       'pickup_loc_name', 'driver_givenname',
+                                       'pickup_datetime'])
             return df
         else:
-            trips = pd.read_csv(os.path.join(self.data_dir, 'trips.csv'))
-            drivers = pd.read_csv(os.path.join(self.data_dir, 'drivers.csv'))
-            pu_locations = pd.read_csv(os.path.join(self.data_dir, 'locations.csv'))
-            do_locations = pd.read_csv(os.path.join(self.data_dir, 'locations.csv'))
-            pu_locations.rename(columns={'location_id': 'pickup_loc_id'}, inplace=True)
-            do_locations.rename(columns={'location_id': 'dropoff_loc_id'}, inplace=True)
+            trips = pd.read_csv(os.path.join(self.data_dir,
+                                             'trips.csv'))
+            drivers = pd.read_csv(os.path.join(self.data_dir,
+                                               'drivers.csv'))
+            pu_locations = pd.read_csv(os.path.join(self.data_dir,
+                                                    'locations.csv'))
+            do_locations = pd.read_csv(os.path.join(self.data_dir,
+                                                    'locations.csv'))
+            pu_locations.rename(columns={'location_id': 'pickup_loc_id'},
+                                inplace=True)
+            do_locations.rename(columns={'location_id': 'dropoff_loc_id'},
+                                inplace=True)
 
             df = pd.merge(trips, drivers, on='driver_id')
-            df = pd.merge(df, pu_locations, on='pickup_loc_id').rename(columns={'loc_name': 'pickup_loc_name'})
-            df = pd.merge(df, do_locations, on='dropoff_loc_id').rename(columns={'loc_name': 'dropoff_loc_name'})
+            df = (pd.merge(df, pu_locations, on='pickup_loc_id')
+                  .rename(columns={'loc_name': 'pickup_loc_name'}))
+            df = (pd.merge(df, do_locations, on='dropoff_loc_id')
+                  .rename(columns={'loc_name': 'dropoff_loc_name'}))
 
             df.rename(columns={
                 'last_name': 'driver_lastname',
@@ -64,28 +79,36 @@ class SakayDB():
 
     def generate_statistics(self, stat):
         """
-        Function will generate different statistics based on the stat
+        Function will generate different
+        statistics based on the stat
         parameter passed.
-        
+
         Parameters
         ----------
         stat : str
-            trip : Returns the average number of trips with pickups per day.
-            
-            passenger : Returns the average number of trips per day per passenger count.
-            
-            driver : Returns the average number of trips per day per driver.
-            
-            all : Keys for dict are trip, passenger, and driver with the corresponding stat.
+            trip : Returns the average number
+            of trips with pickups per day.
+
+            passenger : Returns the average number of
+            trips per day per passenger count.
+
+            driver : Returns the average number of
+            trips per day per driver.
+
+            all : Keys for dict are trip, passenger,
+            and driver with the corresponding stat.
 
         Returns
         -------
         dict
             Dictionary containing the required stats.
         """
-        if (not os.path.isfile(os.path.join(self.data_dir, 'trips.csv'))
-            or not os.path.isfile(os.path.join(self.data_dir, 'drivers.csv'))
-            or not os.path.isfile(os.path.join(self.data_dir, 'locations.csv'))):
+        if (not os.path.isfile(os.path.join(self.data_dir,
+                                            'trips.csv'))
+            or not os.path.isfile(os.path.join(self.data_dir,
+                                               'drivers.csv'))
+            or not os.path.isfile(os.path.join(self.data_dir,
+                                               'locations.csv'))):
 
             if stat in ['trip', 'passenger', 'driver']:
                 return {}
@@ -93,16 +116,24 @@ class SakayDB():
                 return {'trip': {}, 'passenger': {}, 'driver': {}}
             else:
                 raise SakayDBError
-        trips = pd.read_csv(os.path.join(self.data_dir, 'trips.csv'))
-        drivers = pd.read_csv(os.path.join(self.data_dir, 'drivers.csv'))
-        pu_locations = pd.read_csv(os.path.join(self.data_dir, 'locations.csv'))
-        do_locations = pd.read_csv(os.path.join(self.data_dir, 'locations.csv'))
-        pu_locations.rename(columns={'location_id': 'pickup_loc_id'}, inplace=True)
-        do_locations.rename(columns={'location_id': 'dropoff_loc_id'}, inplace=True)
+        trips = pd.read_csv(os.path.join(self.data_dir,
+                                         'trips.csv'))
+        drivers = pd.read_csv(os.path.join(self.data_dir,
+                                           'drivers.csv'))
+        pu_locations = pd.read_csv(os.path.join(self.data_dir,
+                                                'locations.csv'))
+        do_locations = pd.read_csv(os.path.join(self.data_dir,
+                                                'locations.csv'))
+        pu_locations.rename(columns={'location_id': 'pickup_loc_id'},
+                            inplace=True)
+        do_locations.rename(columns={'location_id': 'dropoff_loc_id'},
+                            inplace=True)
 
         df = pd.merge(trips, drivers, on='driver_id')
-        df = pd.merge(df, pu_locations, on='pickup_loc_id').rename(columns={'loc_name': 'pickup_loc_name'})
-        df = pd.merge(df, do_locations, on='dropoff_loc_id').rename(columns={'loc_name': 'dropoff_loc_name'})
+        df = (pd.merge(df, pu_locations, on='pickup_loc_id')
+              .rename(columns={'loc_name': 'pickup_loc_name'}))
+        df = (pd.merge(df, do_locations, on='dropoff_loc_id')
+              .rename(columns={'loc_name': 'dropoff_loc_name'}))
         df.rename(columns={
                 'last_name': 'driver_lastname',
                 'given_name': 'driver_givenname'
@@ -118,29 +149,41 @@ class SakayDB():
                  'trip_distance',
                  'fare_amount']]
 
-        df.pickup_datetime = pd.to_datetime(df.pickup_datetime, format='%H:%M:%S,%d-%m-%Y')
-        df.dropoff_datetime = pd.to_datetime(df.dropoff_datetime, format='%H:%M:%S,%d-%m-%Y')
+        df.pickup_datetime = (pd.to_datetime(df.pickup_datetime,
+                                             format='%H:%M:%S,%d-%m-%Y'))
+        df.dropoff_datetime = (pd.to_datetime(df.dropoff_datetime,
+                                              format='%H:%M:%S,%d-%m-%Y'))
 
         df['day'] = df['pickup_datetime'].dt.day_name()
         dfc = df.copy()
 
         # para case insensitive:
         dfc['day'] = dfc['day'].str.title()
-        dfc['driver'] = dfc['driver_lastname'].str.title()+', '+dfc['driver_givenname'].str.title()
+        dfc['driver'] = (dfc['driver_lastname']
+                         .str.title() +
+                         ', ' +
+                         dfc['driver_givenname']
+                         .str.title())
 
-        week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-        if (stat != 'trip') & (stat != 'passenger') & (stat != 'driver') & (stat != 'all'):
+        week = ['Monday', 'Tuesday',
+                'Wednesday', 'Thursday',
+                'Friday', 'Saturday', 'Sunday']
+        if ((stat != 'trip') &
+                (stat != 'passenger') &
+                (stat != 'driver') &
+                (stat != 'all')):
             raise SakayDBError
 
         elif stat == 'trip':
             trip_ = {}
             for w in week:
                 dfc_filter = (dfc.groupby(['day',
-                                           pd.Grouper(key='pickup_datetime', freq='D')])
+                                           pd.Grouper(key='pickup_datetime',
+                                                      freq='D')])
                               ['trip_id']
                               .count().reset_index())
                 dfc_filter_trip = dfc_filter.groupby('day')['trip_id'].mean()
-                trip = {w:dfc_filter_trip[w]}
+                trip = {w: dfc_filter_trip[w]}
                 trip_.update(**trip)
             return trip_
 
@@ -153,9 +196,18 @@ class SakayDB():
                 # Filter df by unique passenger count
                 dfc_filter = dfc.loc[dfc['passenger_count'] == n]
                 # Daily bins
-                dfc_filter = dfc_filter.groupby(['day', pd.Grouper(key='pickup_datetime', freq='D')]).count().reset_index()
+                dfc_filter = (dfc_filter
+                              .groupby(['day',
+                                        pd.Grouper(key='pickup_datetime',
+                                                   freq='D')])
+                              .count()
+                              .reset_index())
                 # Get mean by day name
-                dfc_filter_pass = dfc_filter.groupby('day')['passenger_count'].mean().reindex(week)
+                dfc_filter_pass = (dfc_filter
+                                   .groupby('day')
+                                   ['passenger_count']
+                                   .mean()
+                                   .reindex(week))
                 # Convert to dictionary
                 daily_ = dfc_filter_pass.to_dict()
                 daily_.update(daily_, **daily_)
@@ -170,9 +222,17 @@ class SakayDB():
                 # Filter by driver name
                 dfc_filter = dfc.loc[dfc['driver'] == n]
                 # Daily bins
-                dfc_filter = dfc_filter.groupby(['day', pd.Grouper(key='pickup_datetime', freq='D')]).count().reset_index()
+                dfc_filter = (dfc_filter
+                              .groupby(['day',
+                                        pd.Grouper(key='pickup_datetime',
+                                                   freq='D')])
+                              .count()
+                              .reset_index())
                 # Get mean by day name
-                dfc_filter_driver = dfc_filter.groupby('day')['driver'].mean().reindex(week)
+                dfc_filter_driver = (dfc_filter
+                                     .groupby('day')
+                                     ['driver']
+                                     .mean().reindex(week))
                 daily_ = dfc_filter_driver.to_dict()
                 daily_.update(daily_, **daily_)
                 driver_[n] = daily_
@@ -181,9 +241,15 @@ class SakayDB():
         elif stat == 'all':
             trip_ = {}
             for w in week:
-                dfc_filter = dfc.groupby(['day', pd.Grouper(key='pickup_datetime', freq='D')])['trip_id'].count().reset_index()
+                dfc_filter = (dfc
+                              .groupby(['day',
+                                        pd.Grouper(key='pickup_datetime',
+                                                   freq='D')])
+                              ['trip_id']
+                              .count()
+                              .reset_index())
                 dfc_filter_trip = dfc_filter.groupby('day')['trip_id'].mean()
-                trip = {w:dfc_filter_trip[w]}
+                trip = {w: dfc_filter_trip[w]}
                 trip_.update(**trip)
 
             number = dfc['passenger_count'].unique()
@@ -194,9 +260,18 @@ class SakayDB():
                 # Filter df by unique passenger count
                 dfc_filter = dfc.loc[dfc['passenger_count'] == n]
                 # Daily bins
-                dfc_filter = dfc_filter.groupby(['day', pd.Grouper(key='pickup_datetime', freq='D')]).count().reset_index()
+                dfc_filter = (dfc_filter
+                              .groupby(['day',
+                                        pd.Grouper(key='pickup_datetime',
+                                                   freq='D')])
+                              .count()
+                              .reset_index())
                 # Get mean by day name
-                dfc_filter_pass = dfc_filter.groupby('day')['passenger_count'].mean().reindex(week)
+                dfc_filter_pass = (dfc_filter
+                                   .groupby('day')
+                                   ['passenger_count']
+                                   .mean()
+                                   .reindex(week))
                 # Convert to dictionary
                 daily_ = dfc_filter_pass.to_dict()
                 daily_.update(daily_, **daily_)
@@ -209,14 +284,24 @@ class SakayDB():
                 # Filter by driver name
                 dfc_filter = dfc.loc[dfc['driver'] == n]
                 # Daily bins
-                dfc_filter = dfc_filter.groupby(['day', pd.Grouper(key='pickup_datetime', freq='D')]).count().reset_index()
+                dfc_filter = (dfc_filter
+                              .groupby(['day',
+                                        pd.Grouper(key='pickup_datetime',
+                                                   freq='D')])
+                              .count()
+                              .reset_index())
                 # Get mean by day name
-                dfc_filter_driver = dfc_filter.groupby('day')['driver'].mean().reindex(week)
+                dfc_filter_driver = (dfc_filter
+                                     .groupby('day')['driver']
+                                     .mean()
+                                     .reindex(week))
                 daily_ = dfc_filter_driver.to_dict()
                 daily_.update(daily_, **daily_)
                 driver_[n] = daily_
 
-            dict_all = {'trip':trip_, 'passenger':passenger_, 'driver':driver_}
+            dict_all = {'trip': trip_,
+                        'passenger': passenger_,
+                        'driver': driver_}
             return dict_all
 
     def plot_statistics(self):
